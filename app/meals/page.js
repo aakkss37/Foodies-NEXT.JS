e0affcf3a408571a105b2e537ea188ea082e77f7
amar kumar <amarkumar.sharma.124@gmail.com>
2023-12-26 14:27:31 +0530

    community page created

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,8 +1,13 @@
 import React from 'react'
+import classes from './page.module.css'
 
 const MealsPage = () => {
     return (
-        <div>MealsPage</div>
+        <div>
+            <header className={classes.header}>
+                <h1>Delicious meals, created{' '} <span className={classes.highlight}>by you</span></h1>
+            </header>
+        </div>
     )
 }
 
