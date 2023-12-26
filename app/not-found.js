32ba7e6adc7b1ecfdb7defb9c022a6aa6003d5b4
amar kumar <amarkumar.sharma.124@gmail.com>
2023-12-26 16:13:20 +0530

    added not found page

---
+++ app/not-found.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const NotFound = () => {
+    return (
+        <main className='not-found'>
+            <h1>Not found</h1>
+            <p>Unfortunately, we couldn&apos;t find the page or resource you are requested for. </p>
+        </main>
+    )
+}
+
+export default NotFound
