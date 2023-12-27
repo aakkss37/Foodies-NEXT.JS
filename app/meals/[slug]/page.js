e090956508dc5672675641e54d7d3d898a85b485
amar kumar <amarkumar.sharma.124@gmail.com>
2023-12-27 19:59:27 +0530

    Meal detail page complete.

--- app/meals/[slug]/page.js
+++ app/meals/[slug]/page.js
@@ -1,8 +1,31 @@
 import React from 'react'
+import classes from './page.module.css'
+import Image from 'next/image'
+import { getMeal } from '@/lib/meals'
 
-const MealsDetailPage = () => {
+const MealsDetailPage = ({ params }) => {
+    const meal = getMeal(params.slug)
+    meal.instructions = meal.instructions.replace(/\n/g, '<br />')
+    const dynamicMarkup = { __html: meal.instructions };
     return (
-        <div>MealsDetailPage</div>
+        <div>
+            <header className={classes.header}>
+                <div className={classes.image}>
+                    <Image src={meal.image} alt={meal.title} fill />
+                </div>
+
+                <div className={classes.headerText}>
+                    <h1>{meal.title}</h1>
+                    <p className={classes.creator}>
+                        by <a href={`mailto: ${meal.creator_email}`}>{meal.creator}</a>
+                    </p>
+                    <p className={classes.summary}>{meal.summary}</p>
+                </div>
+            </header>
+            <main>
+                <p className={classes.instructions} dangerouslySetInnerHTML={dynamicMarkup}></p>
+            </main>
+        </div>
     )
 }
 
